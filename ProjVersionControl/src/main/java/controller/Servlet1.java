14ed06361b7eac02b4ec7229eb262ffc2928a36b
翁光志 <@III-ITC>
2016-06-29 15:46:45 +0800

    新增Servlet1註解

--- ProjVersionControl/src/main/java/controller/Servlet1.java
+++ ProjVersionControl/src/main/java/controller/Servlet1.java
@@ -14,7 +14,7 @@ public class Servlet1 extends HttpServlet {
 	private SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		//這是註解1
 		System.out.println("Servlet1");
 		
 		HttpSession session = request.getSession();
